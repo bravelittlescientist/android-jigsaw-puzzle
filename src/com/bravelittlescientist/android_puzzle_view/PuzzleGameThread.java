781913c593966dca474f2516323ab2ca86225288
Eugenia Gabrielova <genia.likes.science@gmail.com>
2013-02-20 02:36:03 -0800

    Moved Puzzle partitioning logic to its own class

--- src/com/bravelittlescientist/android_puzzle_view/PuzzleGameThread.java
+++ src/com/bravelittlescientist/android_puzzle_view/PuzzleGameThread.java
@@ -17,20 +17,6 @@ public class PuzzleGameThread extends Thread {
     private Context puzzleContext;
     private boolean running = false;
 
-    // Puzzle Dimensions
-    protected static int puzzleXDimension;
-    protected static int puzzleYDimension;
-
-    protected static int puzzlePieceHeight;
-    protected static int puzzlePieceWidth;
-
-    protected static int puzzleGridX;
-    protected static int puzzleGridY;
-
-    private Bitmap puzzleResult;
-    private Bitmap[] puzzlePiecesArray;
-    private int[][] puzzlePieceTargetPositions;
-
     // Game state
     private int puzzleState;
     public static final int STATE_READY = 1;
@@ -59,7 +45,6 @@ public class PuzzleGameThread extends Thread {
         puzzleFrameLinePaint.setAntiAlias(true);
         puzzleFrameLinePaint.setARGB(120, 120, 180, 0);
 
-        loadPuzzleResources();
     }
 
     public void startPuzzle () {
@@ -151,135 +136,6 @@ public class PuzzleGameThread extends Thread {
 
     /** Managing Puzzle Resources **/
 
-    public void loadPuzzleResources () {
 
-        long targetWidth =  450; // TODO hardcoded for testing
-        long targetHeight = 300; // TODO hardcoded for testing
-        Bitmap decodedPuzzleResource = decodePuzzleBitmapFromResource(
-                puzzleContext.getResources(), R.drawable.kitten_large, targetWidth, targetHeight);
-
-        setPuzzleResource(decodedPuzzleResource);
-    }
-
-    /**
-     * decodePuzzleBitmapFromResource
-     * @param res
-     * @param resId
-     * @param targetWidth
-     * @param targetHeight
-     * @return Bitmap
-     *
-     *  Bitmap Loading Code from Android Developer lesson: "Loading Large Bitmaps Efficiently"
-     */
-    public static Bitmap decodePuzzleBitmapFromResource (
-            Resources res, int resId, long targetWidth, long targetHeight) {
-
-        // Load only the dimensions of the puzzle image
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(res, resId, options);
-
-        // Calculate ratio to scale puzzle bitmap
-        options.inSampleSize = calculateScaledPuzzleSize(options, targetWidth, targetHeight);
-
-        // Decode puzzle resource image to bitmap from computed ratio
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeResource(res, resId, options);
-    }
-
-    /**
-     * calculateScaledPuzzleSize
-     *
-     * Adapted from Android Developer lesson: "Loading Large Bitmaps Efficiently"
-     */
-    public static int calculateScaledPuzzleSize (
-            BitmapFactory.Options options, long targetWidth, long targetHeight) {
-
-        // Source Image Dimensions
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int imageScaleRatio = 1;
-
-        if (height > targetHeight || width > targetWidth) {
-            // Calculate ratios of height and width to target height and width
-            final int heightRatio = Math.round((float) height / (float) targetHeight);
-            final int widthRatio = Math.round((float) width / (float) targetWidth);
-
-            // Choose the smallest ratio as inSampleSize value, this will guarantee
-            // a final image with both dimensions larger than or equal to the
-            // requested height and width.
-            imageScaleRatio = heightRatio < widthRatio ? heightRatio : widthRatio;
-        }
-
-        return imageScaleRatio;
-    }
-
-    /**
-     * setPuzzleResource
-     * @param bm    Memory-scaled bitmap representing puzzle target.
-     *
-     * TODO: Make sure input image is valid.
-     */
-    public void setPuzzleResource(Bitmap bm) {
-        puzzleResult = bm;
-        puzzleXDimension = bm.getWidth();
-        puzzleYDimension = bm.getHeight();
-
-        // TODO: This should only happen here if user did not initialize PuzzleView with a grid
-        buildDynamicPuzzleGrid();
-    }
-
-    /**
-     * buildDynamicPuzzleGrid
-     *
-     * If not already set, computes optimal puzzle piece size using Greatest Common Divisor.
-     * Computes Bitmaps for each piece and sets their target positions.
-     *
-     * TODO: Handle case where GCD is 1, or if piece should be a rectangle?
-     */
-    public void buildDynamicPuzzleGrid() {
-        // Compute optimal piece size:
-        int optimalPieceSize = greatestCommonDivisor(puzzleXDimension, puzzleYDimension);
-
-        // Update puzzle dimension variables
-        puzzlePieceHeight = optimalPieceSize;
-        puzzlePieceWidth = optimalPieceSize;
-        puzzleGridX = puzzleXDimension / puzzlePieceWidth;
-        puzzleGridY = puzzleYDimension / puzzlePieceHeight;
-
-        // Initialize and fill puzzle
-        puzzlePieceTargetPositions = new int[puzzleGridX][puzzleGridY];
-        puzzlePiecesArray = new Bitmap[puzzleGridX * puzzleGridY];
-
-        // Generate array of bitmaps
-        int counter = 0;
-        for (int w = 0; w < puzzleGridX; w++) {
-            for (int h = 0; h < puzzleGridY; h++) {
-                puzzlePiecesArray[counter] = Bitmap.createBitmap(puzzleResult,
-                        w*puzzlePieceWidth, h*puzzlePieceHeight, puzzlePieceWidth, puzzlePieceHeight);
-
-                puzzlePieceTargetPositions[w][h] = counter;
-
-                counter++;
-            }
-        }
-    }
-
-    public int greatestCommonDivisor (int n1, int n2) {
-        if (n2 == 0) return n1;
-        return greatestCommonDivisor(n2, n1 % n2);
-    }
-
-    public Bitmap[] getPuzzlePiecesArray () {
-        return puzzlePiecesArray;
-    }
-
-    public int[] getPuzzleDimensions () {
-        return new int[] { puzzleXDimension, puzzleYDimension, puzzleGridX, puzzleGridY };
-    }
-
-    public int[][] getPuzzlePieceTargetPositions () {
-        return puzzlePieceTargetPositions;
-    }
 
 }
